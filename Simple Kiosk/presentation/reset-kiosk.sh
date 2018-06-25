01f98e0ceb34a83e0cbf77730af616c8dbf294e5
Daniel Stock <daniel@vollstock.de>
2018-06-25 10:24:40 +0200

    Disabled ugly material design for touch history navigation.

--- Simple Kiosk/presentation/reset-kiosk.sh
+++ Simple Kiosk/presentation/reset-kiosk.sh
@@ -4,7 +4,21 @@ URL="file:///assets/data/index.html"
 
 # List of Chromium Command Line Switches
 # https://peter.sh/experiments/chromium-command-line-switches/
-COMMAND="chromium-browser --kiosk --bwsi --incognito --class=TooloopKiosk --disable-infobars --no-default-browser-check --no-first-run --noerrdialogs $URL"
+COMMAND="chromium-browser \
+--kiosk \
+--bwsi \
+--overscroll-history-navigation=1 \
+--incognito \
+--disable-infobars \
+--disable-translate \
+--no-default-browser-check \
+--no-first-run \
+--disable-translate-new-ux \
+--num-raster-threads=4 \
+--enable-zero-copy \
+--noerrdialogs \
+--class=TooloopKiosk \
+$URL"
 
 if [ $EUID == 0 ]; then
     pkill chromium
